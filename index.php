2769825af346dbf09dcc0246b413b416b9de0f73
MatteoStrazzera <mattestrazz@gmail.com>
2024-04-19 14:52:48 +0200

    Add echo to print the values on page

--- index.php
+++ index.php
@@ -21,6 +21,11 @@ $favMovie2 = new Production('The Hateful Eight', 'English', 8);
 $favMovie3 = new Production('Gangs of New York', 'English', 9);
 
 var_dump($favMovie1, $favMovie2, $favMovie3);
+
+echo ('Movie name: ' . $favMovie1->title . ' - Movie lang: ' . $favMovie1->language . ' - Movie rating: ' . $favMovie1->rating . ' / 10' . '<br>');
+echo ('Movie name: ' . $favMovie2->title . ' - Movie lang: ' . $favMovie2->language . ' - Movie rating: ' . $favMovie2->rating . ' / 10' . '<br>');
+echo ('Movie name: ' . $favMovie3->title . ' - Movie lang: ' . $favMovie3->language . ' - Movie rating: ' . $favMovie3->rating . ' / 10');
+
 ?>
 
 
