e4bccda83a78c8b8b545e5d1edea22d9e9b7fc85
MatteoStrazzera <mattestrazz@gmail.com>
2024-04-19 17:45:49 +0200

    Edit classes with extends to Production.php

--- models/Movies.php
+++ models/Movies.php
@@ -1,13 +1,14 @@
 <?php
 
-class Movies
+class Movies extends Production
 {
 
     public $profits;
     public $duration;
 
-    public function __construct(int $profits, int $duration)
+    public function __construct(string $title, string $language, int $rating, int $profits, int $duration)
     {
+        parent::__construct($title, $language, $rating);
         $this->profits = $profits;
         $this->duration = $duration;
     }
--- models/TvSerie.php
+++ models/TvSerie.php
@@ -1,12 +1,13 @@
 <?php
 
-class TvSerie
+class TvSerie extends Production
 {
 
     public $totalSeason;
 
-    public function __construct(int $totalSeason)
+    public function __construct(string $title, string $language, int $rating, int $totalSeason)
     {
+        parent::__construct($title, $language, $rating);
         $this->totalSeason = $totalSeason;
     }
 }
